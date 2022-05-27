7be8c43369832a0e14904f52b6fa620b10abc54d
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2022-05-27 10:26:22 -0300

    Readme. New Scene created

---
+++ E404-Tech-Unity-Test/Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _pointsToWin = 100;
+    [SerializeField] private int _secondsToLose = 120;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
